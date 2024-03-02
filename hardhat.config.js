ed46cc528491b88a3c5e34a6f427727bb2caccaf
Edward Kim <itgma1231@gmail.com>
2024-03-02 14:25:18 -0500

    hardhat.config.js/vite.config.js setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.117',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
