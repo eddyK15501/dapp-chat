96518c4dac8a5d0923a5f02a5a96ebd544ea50f5
Edward Kim <itgma1231@gmail.com>
2024-03-02 19:45:53 -0500

    struct and mapping data types defined; function createChannel()

--- contracts/Dappchat.sol
+++ contracts/Dappchat.sol
@@ -7,10 +7,24 @@ import "hardhat/console.sol";
 
 contract Dappchat is ERC721 {
     address public owner;
+    uint256 public index;
+
+    struct Channel {
+        uint256 id;
+        string name;
+        uint256 cost;
+    }
+
+    mapping(uint256 => Channel) public channels;
 
     constructor(string memory _name, string memory _symbol) 
         ERC721(_name, _symbol) 
     {
         owner = msg.sender;
+    }
+    
+    function createChannel(string memory _name, uint256 _cost) public {
+        index += 1;
+        channels[index] = Channel(index, _name, _cost);
     }
 }
