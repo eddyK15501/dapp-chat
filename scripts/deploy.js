319e647c2a4b67fd26fdf0181db2c8ea49359376
Edward Kim <itgma1231@gmail.com>
2024-03-06 11:21:49 -0500

    deploy.js - set local variables/deploy smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,21 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {}
+async function main() {
+  // Variables
+  const NAME = 'dAppchat';
+  const SYMBOL = 'CHAT';
+  const signer = await ethers.getSigners();
+
+  // Deploy contract
+  const DappchatFactory = await ethers.getContractFactory('Dappchat');
+  dappchat = await DappchatFactory.deploy(NAME, SYMBOL);
+  await dappchat.deployed();
+
+  console.log(`Dappchat Contract deployed at: ${dappchat.address}`);
+
+  
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
