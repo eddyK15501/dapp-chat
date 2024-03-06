a7a27ebc95ee45737a910e46a560d16d4b4acf71
Edward Kim <itgma1231@gmail.com>
2024-03-06 17:29:30 -0500

    App.js - fetchContractABI() in useEffect; get mock hardhat MetaMask accounts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Navigation from '../components/Navigation'
+import { useEffect } from 'react';
+import { ethers } from 'ethers';
+import Navigation from '../components/Navigation';
 
-import config from '../config/config.json'
+import config from '../config/config.json';
 
 function App() {
+  const fetchContractABI = async () => {
+    // Get MetaMask account
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    console.log(account);
+  };
+
+  useEffect(() => {
+    fetchContractABI();
+  }, []);
+
   return (
     <>
       <div>
@@ -12,7 +26,7 @@ function App() {
         <main></main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
