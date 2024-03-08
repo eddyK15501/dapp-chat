d10d7c86198d364ab3dd723ac28acd94097da481
Edward Kim <itgma1231@gmail.com>
2024-03-08 18:15:44 -0500

    Channels.jsx - render channels as <li></li>

--- client/src/components/Channels.jsx
+++ client/src/components/Channels.jsx
@@ -1,8 +1,17 @@
-const Channels = () => {
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Channels = ({ account, provider, contract, channels }) => {
   return (
     <div className='channels'>
       <div className='channels__text'>
         <h2>Text Channels</h2>
+        <ul>
+          {channels.map((channel) => {
+            return <li key={channel[0].toString()}>{channel.name}</li>;
+          })}
+        </ul>
       </div>
       <div className='channels__voice'>
         <h2>Voice Channels</h2>
