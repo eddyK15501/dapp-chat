17e2c522ccea099747842013938d8e45ecba61a2
Edward Kim <itgma1231@gmail.com>
2024-03-10 17:47:15 -0400

    Channels.jsx - check onClick() if msg.sender address hasJoined()

--- client/src/components/Channels.jsx
+++ client/src/components/Channels.jsx
@@ -3,13 +3,30 @@
 import React from 'react';
 
 const Channels = ({ account, provider, contract, channels }) => {
+  const handleChannel = async (channel) => {
+    const hasJoined = await contract.hasJoined(channel.id, account);
+
+    if (hasJoined) {
+      console.log("Joined.")
+    } else {
+      console.log("Minting required.")
+    }
+  };
+
   return (
     <div className='channels'>
       <div className='channels__text'>
         <h2>Text Channels</h2>
         <ul>
           {channels.map((channel) => {
-            return <li key={channel[0].toString()}>{channel.name}</li>;
+            return (
+              <li
+                key={channel[0].toString()}
+                onClick={() => handleChannel(channel)}
+              >
+                {channel.name}
+              </li>
+            );
           })}
         </ul>
       </div>
