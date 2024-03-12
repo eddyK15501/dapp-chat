9f6459f141173f0fab19423bf53a786b6ab69a9f
Edward Kim <itgma1231@gmail.com>
2024-03-12 11:44:53 -0400

    express server connected to socket.io; /utils/messages.js added

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
 
+const { messages } = require('./utils/messages.js');
+
 const PORT = process.env.PORT || 3003;
 
 const server = app.listen(PORT, () => {
   console.log(`Express server listening on Port ${PORT}`);
+});
+
+const { Server } = require('socket.io');
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3002',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('get messages', () => {
+    io.emit('get messages', messages);
+  });
+
+  socket.on('new message', (msg) => {
+    messages.push(msg);
+    io.emit('new message', messages);
+  });
 });
